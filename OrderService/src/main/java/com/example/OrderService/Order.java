da9daffc937ab416697bf566f69fc1521ea67147
Дмитрий <tds1991j@gmail.com>
2025-09-01 15:20:10 +0700

    Update Order.java

--- OrderService/src/main/java/com/example/OrderService/Order.java
+++ OrderService/src/main/java/com/example/OrderService/Order.java
@@ -1,4 +1,16 @@
 package com.example.OrderService;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Order {
+    private Long id;
+    private Long userId;
+    private BigDecimal amount;
+    private String currency;
+    private List<String> items;
+
+
+    public <E> Order(long l, Long userId, BigDecimal bigDecimal, String rub, java.util.List<E> ноутбук) {
+    }
 }
