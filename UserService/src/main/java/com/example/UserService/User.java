c2f1ee51c3588636d31aabfc5df2f13bd9219768
Дмитрий <tds1991j@gmail.com>
2025-09-01 15:19:29 +0700

    Update User.java

--- UserService/src/main/java/com/example/UserService/User.java
+++ UserService/src/main/java/com/example/UserService/User.java
@@ -1,4 +1,12 @@
 package com.example.UserService;
 
 public class User {
+    private Long id;
+    private String fullName;
+    private String deliveryAddress;
+    private String phoneNumber;
+    private String email;
+
+    public User(long userId, String иванИванов, String s, String s1, String mail) {
+    }
 }
