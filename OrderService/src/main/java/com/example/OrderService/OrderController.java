7d84cf798fc3233920f05280ec98bfbc7bf80c6e
Дмитрий <tds1991j@gmail.com>
2025-09-01 15:20:24 +0700

    Update OrderController.java

--- OrderService/src/main/java/com/example/OrderService/OrderController.java
+++ OrderService/src/main/java/com/example/OrderService/OrderController.java
@@ -1,4 +1,19 @@
 package com.example.OrderService;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+    @GetMapping("/by-user/{userId}")
+    public List<Order> getOrdersByUser(@PathVariable Long userId) {
+        return List.of(
+                new Order(1L, userId, BigDecimal.valueOf(1000), "RUB", List.of("Ноутбук", "Мышь")),
+        new Order(2L, userId, BigDecimal.valueOf(500), "RUB", List.of("клавиатура"))
+        );
+    }
 }
