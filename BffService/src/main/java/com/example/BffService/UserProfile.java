8df4ad122f08cc30967516a1c1f8f38ca33fad20
Дмитрий <tds1991j@gmail.com>
2025-09-01 15:20:58 +0700

    Update UserProfile.java

--- BffService/src/main/java/com/example/BffService/UserProfile.java
+++ BffService/src/main/java/com/example/BffService/UserProfile.java
@@ -1,4 +1,11 @@
 package com.example.BffService;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
+
 public class UserProfile {
+    private SecurityProperties.User user;
+    private List<Order> orders;
 }
