f006eb4aac89a9234fcc7a440c98b6e34805a4ad
Дмитрий <tds1991j@gmail.com>
2025-09-01 15:19:43 +0700

    Update UserController.java

--- UserService/src/main/java/com/example/UserService/UserController.java
+++ UserService/src/main/java/com/example/UserService/UserController.java
@@ -1,4 +1,14 @@
 package com.example.UserService;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable long userId) {
+        return new User(userId, "Иван Иванов", "ул.Ленина 1", "+79991234567", "ivan@example.com");
+    }
+
 }
